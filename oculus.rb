5500124f5d2a6b70005c37487ecbb865ae094d70
Jacob Beck <jacob@parlor.io>
2026-04-01 08:09:59 -0700

    oculus 0.5.3

--- oculus.rb
+++ oculus.rb
@@ -1,28 +1,28 @@
 class Oculus < Formula
   desc "Go-native AI coding CLI with multi-model lens architecture"
   homepage "https://github.com/howlerops/oculus"
-  version "0.5.2"
+  version "0.5.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.2/oculus_0.5.2_darwin_arm64.tar.gz"
-      sha256 "2eb41ee65cee7fcbfe54c81f0881fe72407f7929413a44ec5b5190bd60199e13"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.3/oculus_0.5.3_darwin_arm64.tar.gz"
+      sha256 "549d3eda78eb7059eef9862fedce2f80df53a128a17760f858a10ae2c3b0b4cd"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.2/oculus_0.5.2_darwin_amd64.tar.gz"
-      sha256 "109c8a0b97e4247e576e0bf0f97b3ec997dcc360ec45a565ad4b42082bf87d2d"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.3/oculus_0.5.3_darwin_amd64.tar.gz"
+      sha256 "5cd4a1a5ded8bb52c9776ed532da13c7e7e1256edea03a53176e3a0919810b7d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.2/oculus_0.5.2_linux_arm64.tar.gz"
-      sha256 "eb3fa3bd12ca76cb71773ab27a290409370c34df451f988d01f704c78bb9050f"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.3/oculus_0.5.3_linux_arm64.tar.gz"
+      sha256 "93df25979dc0b80fa4fcc684738646645dff7bd5565354c9c4137bd9952106d6"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.2/oculus_0.5.2_linux_amd64.tar.gz"
-      sha256 "827b296ed94005ed03f5c01089830a6103862bd36c3face597b321fbc2b52f70"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.3/oculus_0.5.3_linux_amd64.tar.gz"
+      sha256 "edcb5c1a8ba62c5f5f94208c77034eb05d55e13ad322745bfa0b20e9a7dafe75"
     end
   end
 
