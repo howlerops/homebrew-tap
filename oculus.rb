67653530f5bc9f27affb0005fdf6a4cbffef2874
Jacob Beck <jacob@parlor.io>
2026-04-01 07:50:34 -0700

    oculus 0.5.1 - fix forced Anthropic login

--- oculus.rb
+++ oculus.rb
@@ -1,28 +1,28 @@
 class Oculus < Formula
   desc "Go-native AI coding CLI with multi-model lens architecture"
   homepage "https://github.com/howlerops/oculus"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.0/oculus_0.5.0_darwin_arm64.tar.gz"
-      sha256 "226d65e6ed76a8f42fcb43fc47bd4baca45d9a066f502953385401d353a6c4a4"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.1/oculus_0.5.1_darwin_arm64.tar.gz"
+      sha256 "325eefa8a2ff107e4f15842b7507e19c7fe3d3d923255500dd4f8ebdfcc5bbe7"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.0/oculus_0.5.0_darwin_amd64.tar.gz"
-      sha256 "8c3dd7565bda1b67b38a7390be01813dbea148fe393e453dae744f2bc5c015c3"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.1/oculus_0.5.1_darwin_amd64.tar.gz"
+      sha256 "a96357351c59eac44cde9cea589063fd7ccac1ddef36459f18b1e3f596bd9833"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.0/oculus_0.5.0_linux_arm64.tar.gz"
-      sha256 "4b4d36e17c40bcbb51da4ea7c2e16bf0c48ed14678236426a850efe091868213"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.1/oculus_0.5.1_linux_arm64.tar.gz"
+      sha256 "489a6e47352206b745c02e1688a95fc5e03017d7f59df90da9183ad6a3f51137"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.0/oculus_0.5.0_linux_amd64.tar.gz"
-      sha256 "db1e5d0dbb16ab6cdc398b866dafaa370342a20ea51e36ad1df094f54753a675"
+      url "https://github.com/howlerops/oculus/releases/download/v0.5.1/oculus_0.5.1_linux_amd64.tar.gz"
+      sha256 "c732ad95837afb70a3f1bd4ff4ccaf298801a6de1f57205754cdbc4abdb50cf1"
     end
   end
 
