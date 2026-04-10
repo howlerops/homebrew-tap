dccdbf75331db641a3d685f20c17e3b7d4d73318
Jacob Beck <jacobbeck.dev@gmail.com>
2026-04-10 09:53:30 -0700

    Update oculus formula to v0.7.0

--- oculus.rb
+++ oculus.rb
@@ -1,28 +1,28 @@
 class Oculus < Formula
   desc "Go-native AI coding CLI with multi-model lens architecture"
   homepage "https://github.com/howlerops/oculus"
-  version "0.5.5"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.5/oculus_0.5.5_darwin_arm64.tar.gz"
-      sha256 "f3c3f7cd17602089ad6afacb374501d047e915ff881d052d88cea5bdcdfdf297"
+      url "https://github.com/howlerops/oculus/releases/download/v0.7.0/oculus_0.7.0_darwin_arm64.tar.gz"
+      sha256 "30f2de22c8af545cc94e2ee28ec90d8cfe8f77c4fccc2d8e8b459cf95c58ddf7"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.5/oculus_0.5.5_darwin_amd64.tar.gz"
-      sha256 "6f6372789962299b359f8e0e573c034498fbf334001efa825b17ab691ca2c4ef"
+      url "https://github.com/howlerops/oculus/releases/download/v0.7.0/oculus_0.7.0_darwin_amd64.tar.gz"
+      sha256 "eb147660d91dc61bd5e64ca49da932d054696f2281ed93fac272d20934ae6eb8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.5/oculus_0.5.5_linux_arm64.tar.gz"
-      sha256 "77deae356620a3be41dcce4463a6a0c6bc3f609ea30d1bf074e644e6dc378bc3"
+      url "https://github.com/howlerops/oculus/releases/download/v0.7.0/oculus_0.7.0_linux_arm64.tar.gz"
+      sha256 "9e50459bb5acd91c27beb063a07b47d0cba64555f9fd099ee1e122925a939503"
     end
     on_intel do
-      url "https://github.com/howlerops/oculus/releases/download/v0.5.5/oculus_0.5.5_linux_amd64.tar.gz"
-      sha256 "298dfb35980abf60330c0bda2d9ce4e8d827a3aa68fda5aa4e1b3b745c96838c"
+      url "https://github.com/howlerops/oculus/releases/download/v0.7.0/oculus_0.7.0_linux_amd64.tar.gz"
+      sha256 "4955d88b84c148d3fec6e106b059d74ab33d611435f2234df60b1d23321eadd9"
     end
   end
 
